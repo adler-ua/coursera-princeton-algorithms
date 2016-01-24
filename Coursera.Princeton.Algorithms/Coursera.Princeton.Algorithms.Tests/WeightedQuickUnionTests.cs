a35d2f662473539cf417cc739aaf1e4d8308147d
adler-ua <adler.ua@gmail.com>
2016-01-25 00:05:37 +0200

    Implemented WeightedQuickUnion

---
+++ Coursera.Princeton.Algorithms/Coursera.Princeton.Algorithms.Tests/WeightedQuickUnionTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Coursera.Princeton.Algorithms.Part1.Week1;
+
+namespace Coursera.Princeton.Algorithms.Tests
+{
+    [TestClass]
+    public class WeightedQuickUnionTests
+    {
+        [TestMethod]
+        public void InitWorks()
+        {
+            IUnionFind uf = new QuickUnion(10);
+            int[] ids = uf.GetIds();
+            for (int i = 0; i < 10; i++)
+            {
+                Assert.AreEqual(i, ids[i]);
+            }
+        }
+
+        [TestMethod]
+        public void DemoTest()
+        {
+            IUnionFind uf = new WeightedQuickUnion(10);
+
+            int[] root = new int[10];
+
+            uf.Union(4, 3);
+            root = uf.GetIds();
+            Assert.AreEqual(4, root[3]);
+
+            uf.Union(3, 8);
+            root = uf.GetIds();
+            Assert.AreEqual(4, root[8]);
+
+            uf.Union(6, 5);
+            root = uf.GetIds();
+            Assert.AreEqual(6, root[5]);
+
+            uf.Union(9, 4);
+            root = uf.GetIds();
+            Assert.AreEqual(4, root[9]);
+
+            uf.Union(2, 1);
+            root = uf.GetIds();
+            Assert.AreEqual(2, root[1]);
+
+            uf.Union(5, 0);
+            root = uf.GetIds();
+            Assert.AreEqual(6, root[0]);
+
+            uf.Union(7, 2);
+            root = uf.GetIds();
+            Assert.AreEqual(2, root[7]);
+
+            uf.Union(6, 1);
+            root = uf.GetIds();
+            Assert.AreEqual(6, root[2]);
+
+            uf.Union(7, 3);
+            root = uf.GetIds();
+            Assert.AreEqual(6, root[4]);
+
+            Assert.AreEqual(6, root[0]);
+            Assert.AreEqual(2, root[1]);
+            Assert.AreEqual(6, root[2]);
+            Assert.AreEqual(4, root[3]);
+            Assert.AreEqual(6, root[4]);
+            Assert.AreEqual(6, root[5]);
+            Assert.AreEqual(6, root[6]);
+            Assert.AreEqual(2, root[7]);
+            Assert.AreEqual(4, root[8]);
+            Assert.AreEqual(4, root[9]);
+        }
+    }
+}
---
+++ Coursera.Princeton.Algorithms/Coursera.Princeton.Algorithms/Part1/Week1/WeightedQuickUnion.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coursera.Princeton.Algorithms.Part1.Week1
+{
+    public class WeightedQuickUnion : IUnionFind
+    {
+        private int _size;
+        private int[] _root;
+        private int[] _rootsize;
+
+        public WeightedQuickUnion(int size)
+        {
+            Init(size);
+        }
+
+        private void Init(int size)
+        {
+            _size = size;
+            _root = new int[_size];
+            _rootsize = new int[_size];
+            for(int i=0;i< _size;i++)
+            {
+                _root[i] = i;
+                _rootsize[i] = 1;
+            }
+        }
+
+        public bool Connected(int p, int q)
+        {
+            return Root(p) == Root(q);
+        }
+
+        public int[] GetIds()
+        {
+            return _root;
+        }
+
+        public void Union(int p, int q)
+        {
+            int pRoot = Root(p);
+            int qRoot = Root(q);
+            if (_rootsize[qRoot] > _rootsize[pRoot])
+            {
+                _root[pRoot] = _root[qRoot];
+                _rootsize[qRoot] += _rootsize[pRoot];
+            }
+            else
+            {
+                _root[qRoot] = _root[pRoot];
+                _rootsize[pRoot] += _rootsize[qRoot];
+            }
+        }
+
+        private int Root(int p)
+        {
+            while (p != _root[p]) p = _root[p];
+            return p;
+        }
+    }
+}
